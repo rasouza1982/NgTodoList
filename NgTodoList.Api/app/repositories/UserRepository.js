1975e01425c0918138cfa14e3e24dfdc89d71801
rasouza1982 <souza.rafael@gmail.com>
2017-03-22 21:28:50 -0300

    update

---
+++ NgTodoList.Api/app/repositories/UserRepository.js
@@ -0,0 +1,50 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .factory('UserRepository', UserRepository);
+
+    UserRepository.$inject = ['$http', '$rootScope', '$location'];
+
+    function UserRepository($http, $rootScope, $location) {
+        return {
+            setCurrentProfile: function () {
+                return $http.get("/api/account/profile", { headers: { 'Authorization': 'Bearer ' + localStorage.getItem('token') } })
+                    .then(
+                    function (result) {
+                        $rootScope.user = {
+                            guid: result.data.guid,
+                            name: result.data.name,
+                            email: result.data.email,
+                            image: result.data.image,
+                            username: result.data.username,
+                            theme: result.data.theme
+                        };
+                    },
+                    function (error) {
+                        ClearUserData();
+                    });
+            },
+            clearUserData: function () {
+                ClearUserData();
+            },
+            register: function (user) {
+                return $http.post("/api/account/register", user, { headers: { 'Authorization': 'Bearer ' + localStorage.getItem('token') } });
+            },
+            resetPassword: function (email) {
+                return $http.post("/api/account/resetpassword", email, { headers: { 'Authorization': 'Bearer ' + localStorage.getItem('token') } });
+            }
+        };
+
+        function ClearUserData() {
+            $rootScope.isAuthorized = false;
+            $rootScope.user = {
+                name: '',
+                email: ''
+            };
+            localStorage.setItem('token', '');
+            $location.path('/login');
+        }
+    }
+})();
